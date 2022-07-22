144fe521539dca65576a2e3d21908f6c38d68858
Tyler Simons <39609292+tyler-simons@users.noreply.github.com>
2022-07-21 22:30:06 -0700

    delete files in timelapse folder

--- sunrise_tl.py
+++ sunrise_tl.py
@@ -9,13 +9,13 @@ from google.oauth2 import service_account
 
 
 print("Starting timelapse camera")
-time_start = 5
 time_end = 8
 photo_folder = "/home/pi/sunrises/"
 GCP_KEY_PATH = "./tylerpersonalprojects-362a1ae72b01.json"
 GCP_PROJECT = "tylerpersonalprojects"
 GCP_GCS_BUCKET = "timelapses"
-delete_folder = False
+delete_folder = True
+timelapse_wait = 120
 
 # Clear out the folder
 if delete_folder:
@@ -34,18 +34,15 @@ while int(time_end) > dtdt.now().hour:
     camera = PiCamera()
     camera.resolution = (1024, 768)
     camera.start_preview()
-    # Camera warm-up time
     timestamp = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
     camera.annotate_text = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     time.sleep(2)
     camera.capture(photo_folder+f"{timestamp}.jpg")
     print("Photo Taken")
     camera.close() 
-    time.sleep(120)
+    time.sleep(timelapse_wait)
 
 # Compile into a gif
-images = []
-sorted_pics = sorted(os.listdir(photo_folder))
 
 def getfiles(dirpath):
     a = [s for s in os.listdir(dirpath)
@@ -54,6 +51,7 @@ def getfiles(dirpath):
     return a
 
 # Get sorted files by creation time
+images = []
 sorted_pics = getfiles(photo_folder)
 
 for filename in sorted_pics:
@@ -64,6 +62,7 @@ todays_date = datetime.datetime.now().strftime("%Y%m%d")
 gif_name = f'{todays_date}.gif'
 
 imageio.mimsave(photo_folder + gif_name, images, fps=5)
+
 
 # Push to GCP
 credentials = service_account.Credentials.from_service_account_file(GCP_KEY_PATH)
